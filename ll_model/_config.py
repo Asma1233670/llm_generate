2b6f7b7a3ac881d708909f8f808f5a3be9474ae9
Asma Houimli <75503007+Asma1233670@users.noreply.github.com>
2024-10-05 15:30:43 +0200

    Update _config.py

--- ll_model/_config.py
+++ ll_model/_config.py
@@ -1,6 +1,6 @@
 from ollama import Client
 #if you want to put ollama in a different machine and point to it 
-OLLAMA_HOST="http://ip:11434/"#11434 ollama default port
+OLLAMA_HOST="http://localhost:11434/"#11434 ollama default port
 DEFAULT_MODEL_NAME="llama3"
 
 def Create_client():
