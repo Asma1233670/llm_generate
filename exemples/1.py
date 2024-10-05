370ea162e8d9571650434b34e931eb20d67cc82b
Asma Houimli <75503007+Asma1233670@users.noreply.github.com>
2024-10-05 17:50:53 +0200

    Update 1.py

--- exemples/1.py
+++ exemples/1.py
@@ -1,3 +1,2 @@
 from ll_model.LL_model import LL_model
-
 LL_model.generate(prompt="Give me a document in the domain of sports.",system_message="You're a AI assistant , that will assist me to create different pdfs in different fields to create a dataset that will be used later for other tasks.",output_file="1.pdf")
